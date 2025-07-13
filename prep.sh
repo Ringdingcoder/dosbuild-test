ec27111bfb67099388ac4936631772d8a1e4bf12
Stefan Ring <stefanrin@gmail.com>
2025-07-13 10:28:19 +0200

    doslfn

--- prep.sh
+++ prep.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-cat .mtoolsrc
 mdir X:
 curl -LO 'https://gitlab.com/FreeDOS/base/fdapm/-/raw/master/BIN/FDAPM.COM?ref_type=heads&inline=false'
+curl -L -o doslfn.zip 'http://adoxa.altervista.org/doslfn/dl.php?f=doslfn'
+unzip doslfn.zip
 mcopy FDAPM.COM X:
+mcopy doslfn.com X:
 mcopy config.sys X:
 mcopy autoexec.bat X:
 mcopy run.bat X:
