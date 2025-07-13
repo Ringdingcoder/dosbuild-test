759f82ab138a8dd69cd48735a93cd52fc7f5cbaa
Stefan Ring <stefanrin@gmail.com>
2025-07-13 10:19:34 +0200

    copy files

--- prep.sh
+++ prep.sh
@@ -3,3 +3,6 @@
 mdir X:
 curl -LO 'https://gitlab.com/FreeDOS/base/fdapm/-/raw/master/BIN/FDAPM.COM?ref_type=heads&inline=false'
 mcopy FDAPM.COM X:
+mcopy config.sys X:
+mcopy autoexec.bat X:
+mcopy run.bat X:
